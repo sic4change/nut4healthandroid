b59579e93f3b4fbfdcb2c34fc0108888c9970aba
mac <aaron.asencio.tavio@gmail.com>
2019-05-13 08:29:13 +0100

    working in notification

---
+++ app/src/main/java/org/sic4change/nut4health/notification/FirebaseMessagingService.java
@@ -0,0 +1,31 @@
+package org.sic4change.nut4health.notification;
+
+import android.app.NotificationManager;
+import android.content.Context;
+import android.media.RingtoneManager;
+import android.support.v4.app.NotificationCompat;
+
+import com.google.firebase.messaging.RemoteMessage;
+
+import org.sic4change.nut4health.R;
+
+public class FirebaseMessagingService extends com.google.firebase.messaging.FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(RemoteMessage message) {
+        super.onMessageReceived(message);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, "channel_id")
+                .setContentTitle(message.getNotification().getTitle())
+                .setContentText(message.getNotification().getBody())
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setStyle(new NotificationCompat.BigTextStyle())
+                .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setAutoCancel(true);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0, notificationBuilder.build());
+    }
+}
