e077fe5202df9f842c15caac475cfa9053ee62c8
Aaron Asencio <aaron.asencio.tavio@gmail.com>
2022-09-09 15:20:31 +0100

    export diagnosis with sex field

--- app/src/main/java/org/sic4change/nut4health/ui/main/contracts/ContractFragment.java
+++ app/src/main/java/org/sic4change/nut4health/ui/main/contracts/ContractFragment.java
@@ -248,16 +248,17 @@ public class ContractFragment extends Fragment {
                     Row rowHeader = sheet.createRow(0);
                     rowHeader.createCell(0).setCellValue(getString(R.string.name_export));
                     rowHeader.createCell(1).setCellValue(getString(R.string.surnname_export));
-                    rowHeader.createCell(2).setCellValue(getString(R.string.address_export));
-                    rowHeader.createCell(3).setCellValue(getString(R.string.phone_export));
-                    rowHeader.createCell(4).setCellValue(getString(R.string.date_export));
-                    rowHeader.createCell(5).setCellValue(getString(R.string.date_millis_export));
-                    rowHeader.createCell(6).setCellValue(getString(R.string.screener_export));
-                    rowHeader.createCell(7).setCellValue(getString(R.string.service_export));
-                    rowHeader.createCell(8).setCellValue(getString(R.string.date_health_service));
-                    rowHeader.createCell(9).setCellValue(getString(R.string.date_millis_health_service));
-                    rowHeader.createCell(10).setCellValue(getString(R.string.locatoin_health_service));
-                    rowHeader.createCell(11).setCellValue(getString(R.string.status));
+                    rowHeader.createCell(2).setCellValue(getString(R.string.sex_export));
+                    rowHeader.createCell(3).setCellValue(getString(R.string.address_export));
+                    rowHeader.createCell(4).setCellValue(getString(R.string.phone_export));
+                    rowHeader.createCell(5).setCellValue(getString(R.string.date_export));
+                    rowHeader.createCell(6).setCellValue(getString(R.string.date_millis_export));
+                    rowHeader.createCell(7).setCellValue(getString(R.string.screener_export));
+                    rowHeader.createCell(8).setCellValue(getString(R.string.service_export));
+                    rowHeader.createCell(9).setCellValue(getString(R.string.date_health_service));
+                    rowHeader.createCell(10).setCellValue(getString(R.string.date_millis_health_service));
+                    rowHeader.createCell(11).setCellValue(getString(R.string.locatoin_health_service));
+                    rowHeader.createCell(12).setCellValue(getString(R.string.status));
                     for(int  i=0; i<contracts.size(); i++){
                         Row row = sheet.createRow(i+1);
                         String status = "";
@@ -270,16 +271,17 @@ public class ContractFragment extends Fragment {
                         }
                         row.createCell(0).setCellValue(contracts.get(i).getChildName());
                         row.createCell(1).setCellValue(contracts.get(i).getChildSurname());
-                        row.createCell(2).setCellValue(contracts.get(i).getChildAddress());
-                        row.createCell(3).setCellValue(contracts.get(i).getChildPhoneContract());
-                        row.createCell(4).setCellValue(contracts.get(i).getCreationDate());
-                        row.createCell(5).setCellValue(contracts.get(i).getCreationDateMiliseconds());
-                        row.createCell(6).setCellValue(contracts.get(i).getScreener());
-                        row.createCell(7).setCellValue(contracts.get(i).getMedical());
-                        row.createCell(8).setCellValue(contracts.get(i).getMedicalDate());
-                        row.createCell(9).setCellValue(contracts.get(i).getMedicalDateMiliseconds());
-                        row.createCell(10).setCellValue(contracts.get(i).getPointFullName());
-                        row.createCell(11).setCellValue(status);
+                        row.createCell(2).setCellValue(contracts.get(i).getSex());
+                        row.createCell(3).setCellValue(contracts.get(i).getChildAddress());
+                        row.createCell(4).setCellValue(contracts.get(i).getChildPhoneContract());
+                        row.createCell(5).setCellValue(contracts.get(i).getCreationDate());
+                        row.createCell(6).setCellValue(contracts.get(i).getCreationDateMiliseconds());
+                        row.createCell(7).setCellValue(contracts.get(i).getScreener());
+                        row.createCell(8).setCellValue(contracts.get(i).getMedical());
+                        row.createCell(9).setCellValue(contracts.get(i).getMedicalDate());
+                        row.createCell(10).setCellValue(contracts.get(i).getMedicalDateMiliseconds());
+                        row.createCell(11).setCellValue(contracts.get(i).getPointFullName());
+                        row.createCell(12).setCellValue(status);
                     }
                     File file = new File(getActivity().getExternalFilesDir(null), "contracts.xlsx");
                     try {
