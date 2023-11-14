3469e382f30a210546286184c01e51a2b4e65000
Aaron Asencio <aaron.asencio.tavio@gmail.com>
2023-11-14 11:24:50 +0000

    add dependencies and configurations

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,9 @@
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+    id("dagger.hilt.android.plugin")
 }
 
 android {
@@ -30,11 +33,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
@@ -47,11 +50,14 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+    packagingOptions {
+        exclude("META-INF/gradle/incremental.annotation.processors")
+    }
 }
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.0")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
@@ -59,6 +65,61 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+
+    //Hilt
+    implementation ("com.google.dagger:hilt-android:${project.property("hilt_version")}")
+    implementation ("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation ("com.google.firebase:firebase-config-ktx:21.5.0")
+    implementation ("com.google.dagger:hilt-compiler:${project.property("hilt_version")}")//ojo aqui estaba kapt
+    implementation ("androidx.hilt:hilt-navigation-compose:1.1.0")
+
+    //Firebase
+    implementation(platform("com.google.firebase:firebase-bom:29.1.0"))
+    implementation ("com.google.firebase:firebase-analytics")
+    implementation ("com.google.firebase:firebase-auth:22.2.0")
+    implementation ("com.google.firebase:firebase-storage:20.3.0")
+    implementation ("com.google.firebase:firebase-firestore:24.9.1")
+    implementation ("com.google.firebase:firebase-core:21.1.1")
+    implementation ("com.google.firebase:firebase-messaging:23.3.1")
+    implementation ("com.google.firebase:firebase-firestore-ktx:24.9.1")
+
+    // Either
+    implementation ("io.arrow-kt:arrow-core:1.0.1")
+
+    //Para imagenes
+    implementation ("io.coil-kt:coil-compose:2.3.0")
+    implementation ("androidx.compose.foundation:foundation:1.5.4")
+
+
+    //Retrofit
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    //noinspection GradleDependency
+    implementation("com.squareup.okhttp3:logging-interceptor:4.9.2")
+    implementation ("com.jakewharton.retrofit:retrofit2-kotlin-coroutines-adapter:0.9.2")
+
+    //Moshi
+    implementation ("com.squareup.moshi:moshi:1.6.0")
+
+    //Timber
+    implementation ("com.jakewharton.timber:timber:4.7.1")
+
+    //Dates
+    implementation ("com.jakewharton.threetenabp:threetenabp:1.4.0")
+
+    //Permissions
+    implementation ("com.google.accompanist:accompanist-permissions:${project.property("accompanist_version")}")
+
+    //Camera X
+    implementation("androidx.camera:camera-core:1.3.0")
+    implementation("androidx.camera:camera-camera2:1.3.0")
+    implementation("androidx.camera:camera-lifecycle:1.3.0")
+    implementation("androidx.camera:camera-video:1.3.0")
+    implementation("androidx.camera:camera-view:1.3.0")
+    implementation("androidx.camera:camera-extensions:1.3.0")
+
+    //Stepper
+    //implementation ("com.github.maryamrzdh:compose-stepper:1.0.0-beta01")
 
     //Test
     testImplementation("junit:junit:4.13.2")
